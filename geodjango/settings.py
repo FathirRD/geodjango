ed4faaff58396f164c49d73a98b0a830a30f1702
fath55 <fathirrahmand@gmail.com>
2022-03-23 21:22:46 +0700

    little add

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-aa-s*c0x*j52iit(l(8+t%=ge2ob&$%b_ru4q-nq-1+0(t)e2c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
